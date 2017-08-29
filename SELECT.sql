63fdac0078b14945fb225dc4d9929b059645aabb
mingilab <mingilab@naver.com>
2017-08-29 09:40:23 +0900

    SQL update

--- SELECT.sql
+++ SELECT.sql
@@ -1,3 +1,7 @@
 -- 시퀀스 검색
 select sequence_name from user_sequences;
+select * from PITCHER_STORAGE
 
+insert into ACCOUNT(USERID,USERPW) values('test1','1234');
+insert into HITTER_STORAGE(USERID,PLAYERID,HITTERSEQ) values('test1',164,HITTER_STORAGE_SEQUNCE.nextval)
+insert into PITCHER_STORAGE(USERID,PLAYERID,POSITION,PITCHERSEQ) values('test1',506,'st1',PITCHER_STORAGE_SEQUNCE.nextval)
